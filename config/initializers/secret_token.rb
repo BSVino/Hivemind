81589a51c88173b1969a5bb95ca7f4fceab6314f
Jorge Rodriguez <jorge@lunarworkshop.com>
2011-10-11 00:37:44 -0700

    Initial RoR import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hivemind::Application.config.secret_token = 'bee881ee8d9270aab4dab66d4b32212d685d448f0e2444b18c84e04b3afd1e350d137d83f5df176be253a4c20519a45695358bab427ad973e36b6501a4279cd8'
