075881e107b65cafa5c971bca2879c836e9f9c7f
Jorge Rodriguez <jorge@lunarworkshop.com>
2011-11-09 02:57:49 -0800

    Fix not being able to delete tasks. has_many children is not necessary since deleting projects will delete all children anyway.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :parent, :class_name => "Task", :foreign_key => "parent_id"
-  has_many :children, :class_name => "Task", :dependent => :destroy
 
   validates_presence_of :project
   validates_presence_of :name
